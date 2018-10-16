3e664f093159291b8a1e01cf56afbe0b133010b7
Developerzone01 <fijaz418@gmail.com>
2018-10-16 01:39:58 -0700

    Initial commit

---
+++ app/src/main/java/com/example/faisalijaz/task5_splash_screen/Constants/AppConstants.java
@@ -0,0 +1,5 @@
+package com.example.faisalijaz.task5_splash_screen.Constants;
+
+public class AppConstants {
+    public static final String MESSAGE_INVALID_EXP = "Invalid Expression";
+}
---
+++ app/src/main/java/com/example/faisalijaz/task5_splash_screen/SplashActivity.java
@@ -0,0 +1,33 @@
+package com.example.faisalijaz.task5_splash_screen;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class SplashActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        Thread td = new Thread(){
+            public void run(){
+                try {
+                    sleep(3000);
+                }
+                catch (Exception ex){
+                    ex.printStackTrace();
+                    Toast.makeText(SplashActivity.this,"Unknown error occured",Toast.LENGTH_LONG).show();
+                }
+                finally {
+                    Intent i = new Intent(SplashActivity.this,MainActivity.class);
+                    startActivity(i);
+                }
+            }
+        };
+        td.start();
+
+    }
+}
